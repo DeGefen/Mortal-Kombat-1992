71b865d73c8e21c3524c5e55e3bae1a46981fdfc
DeGefen <gefen.rashty@gmail.com>
2025-05-13 20:30:36 +0300

    feat: enhance player mechanics with new combat and attack systems

--- mortal_kombat_info.h
+++ mortal_kombat_info.h
@@ -145,7 +145,58 @@ namespace mortal_kombat
                 {4,  32,   7688, 230, 220},  // Win
     }};
 
+    static constexpr std::array<CharacterData::CharacterSpriteInfo, CharacterData::SPRITE_SIZE> LIU_KANG_SPRITE_ARRAY{{
+                {8, 32,   58, 230, 220}, // Stance
+                {9,  2138, 58, 230, 220},  // Walk Forwards
+                {9,  2138, 58, 230, 220},  // Walk Backwards
+                {4,  32,   580, 230, 220},  // Low Punch
+                {9, 1202, 580, 230, 220}, // Low Punch Spree
+                {5,  3542, 580, 230, 220},  // Body to Body Punch
+                {4,  32,   1052, 230, 220},  // High Punch
+                {9, 1202, 1052, 230, 220}, // High Punch Spree
+                {5,  3542, 1052, 230, 220},  // Body to Body Kick
+                {7,  32,   1574, 230, 220},  // Low Kick
+                {8,  1904, 1574, 230, 220},  // Lowkick Sweep
+                {9, 32,   2046, 230, 220}, // High Kick
+                {7,  2372, 2046, 230, 220},  // Highkick Sweep
+                {5,  32,   2568, 230, 220},  // Crouch
+                {5,  1436, 2568, 230, 220},  // Uppercut
+                {5,  2840, 2568, 230, 220},  // Crouch Kick
+                {1,  32,   3090, 230, 220},  // Jump
+                {5,  500,  3090, 230, 220},  // Jump Punch
+                {5,  1904, 3090, 230, 220},  // Jump Highkick
+                {3,  3308, 3090, 230, 220},  // Landing
+                {1,  32,   3562, 230, 220},  // Jump Back
+                {7,  500,  3562, 230, 220},  // Roll
+                {5,  2372, 3562, 230, 220},  // Forward Jump Punch
+                {5,  3776, 3562, 230, 220},  // Jump Lowkick
+                {4,  32,   4084, 230, 220},  // Torso Hit
+                {4,  1202, 4084, 230, 220},  // Head Hit
+                {5,  2372, 4084, 230, 220},  // Kickback Torso Hit
+                {3,  3776, 4084, 230, 220},  // Crouch Hit
+                {7,  32,   4606, 230, 220},  // Fall
+                {8,  1904, 4606, 230, 220},  // Uppercut Hit
+                {4,  4010, 4606, 230, 220},  // Nuts Hit
+                {6,  32,   5078, 230, 220},  // Fall Inplace
+                {7,  1670, 5078, 230, 220},  // Getup
+                {7,  32,   5600, 230, 220},  // Caught
+                {7,  1904, 5600, 230, 220},  // Thrown
+                {5,  32,   6122, 230, 220},  // Block
+                {2,  1436, 6122, 230, 220},  // Crouch Block
+                {3,  2138, 6122, 230, 220},  // Turn Right to Left
+                {3,  3074, 6122, 230, 220},  // Turn Left to Right
+                {8, 32,   6644, 230, 220}, // Special 1
+                {4,  2606, 6644, 230, 220},  // Special 2
+                {-1, -1,   -1, 230, 220},  // Special 3
+                {7,  32,   7166, 230, 220},  // Giddy
+                {0,  1904, 7166, 230, 220},  // Finish Him
+                {7,  2606, 7166, 230, 220},  // Giddy Fall
+                {14,  32,   8210, 230, 220},  // Win
+    }};
+
     static constexpr CharacterData SUBZERO_SPRITE(SUBZERO_SPRITE_ARRAY);
+
+    static constexpr CharacterData LIU_KANG_SPRITE(LIU_KANG_SPRITE_ARRAY);
 };
 
 
